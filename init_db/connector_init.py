b20d0999055e23075b8ae520fc352ee26604584a
Rajashekar Varkala <raj@mywavia.com>
2023-03-08 23:41:29 +0530

    check status code instead of exception

--- init_db/connector_init.py
+++ init_db/connector_init.py
@@ -3,21 +3,19 @@ import json
 import requests
 import os
 from requests.auth import HTTPBasicAuth
-import logging
 
 connector_defs = json.loads(open(join(dirname(__file__), "connector_def.json"), "r").read())
 
 for conn_def in connector_defs["definitions"]:
-    try:
-        requests.post(
-            f"http://localhost:{os.environ['PORT']}/api/v1/superuser/connectors/create",
-            json={
-                "type": conn_def["type"].upper() + "_" + conn_def["unique_name"].upper(),
-                "display_name": conn_def["display_name"],
-                "docker_image": conn_def["docker_image"],
-                "docker_tag": conn_def["docker_tag"],
-            },
-            auth=HTTPBasicAuth(os.environ["ADMIN_EMAIL"], os.environ["ADMIN_PASSWORD"]),
-        )
-    except Exception:
-        logging.exception("Failed to create connector. May exist already. Do better - continuing...")
+    resp = requests.post(
+        f"http://localhost:{os.environ['PORT']}/api/v1/superuser/connectors/create",
+        json={
+            "type": conn_def["type"].upper() + "_" + conn_def["unique_name"].upper(),
+            "display_name": conn_def["display_name"],
+            "docker_image": conn_def["docker_image"],
+            "docker_tag": conn_def["docker_tag"],
+        },
+        auth=HTTPBasicAuth(os.environ["ADMIN_EMAIL"], os.environ["ADMIN_PASSWORD"]),
+    )
+    if resp.status_code != 200:
+        print("Failed to create connector. May exist already. Do better - continuing...")
--- init_db/create_db_if_not_exists.py
+++ init_db/create_db_if_not_exists.py
@@ -4,7 +4,7 @@ import os
 
 print(f'CREATING DATABASE {os.environ["DATABASE_URL"]} IF NOT EXISTS')
 result = urlparse(os.environ["DATABASE_URL"])
-#result = urlparse("postgresql://postgres:changeme@localhost/valmi_app")
+# result = urlparse("postgresql://postgres:changeme@localhost/valmi_app")
 username = result.username
 password = result.password
 database = result.path[1:]
@@ -12,20 +12,17 @@ hostname = result.hostname
 port = result.port
 
 try:
-    connection = psycopg2.connect(user = username,
-                    password = password,
-                    host = hostname,
-                    port = port
-                )
-    connection.set_isolation_level(psycopg2.extensions.ISOLATION_LEVEL_AUTOCOMMIT) 
+    connection = psycopg2.connect(user=username, password=password, host=hostname, port=port)
+    connection.set_isolation_level(psycopg2.extensions.ISOLATION_LEVEL_AUTOCOMMIT)
     with connection.cursor() as cursor:
-        cursor.execute(f"SELECT 'CREATE DATABASE {database} WITH OWNER {username} ENCODING ''utf-8''' \
+        cursor.execute(
+            f"SELECT 'CREATE DATABASE {database} WITH OWNER {username} ENCODING ''utf-8''' \
                         WHERE NOT EXISTS \
-                        (SELECT FROM pg_database WHERE datname = '{database}')")
+                        (SELECT FROM pg_database WHERE datname = '{database}')"
+        )
         pg_result = cursor.fetchone()
         if pg_result is not None:
             cursor.execute(pg_result[0])
 finally:
     if connection:
         connection.close()
-
