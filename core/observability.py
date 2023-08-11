b563bcaf9594a6c7622b9a5d3232cc047c27aa57
Rajashekar Varkala <raj@mywavia.com>
2023-08-11 16:39:19 +0530

    Adding log collector through opentelemetry

--- core/observability.py
+++ core/observability.py
@@ -1,10 +1,15 @@
+import logging
 from typing import Iterable
+
+
+from opentelemetry.exporter.otlp.proto.grpc._log_exporter import OTLPLogExporter
+from opentelemetry.sdk._logs.export import BatchLogRecordProcessor
+from opentelemetry.sdk._logs import LoggerProvider, LoggingHandler
+from opentelemetry import _logs
+
 from opentelemetry import trace
-from opentelemetry.propagate import inject
 from opentelemetry.sdk.trace import TracerProvider
-from opentelemetry.exporter.otlp.proto.grpc.metric_exporter import (
-    OTLPMetricExporter
-)
+from opentelemetry.exporter.otlp.proto.grpc.metric_exporter import OTLPMetricExporter
 from opentelemetry.exporter.otlp.proto.grpc.trace_exporter import (
     OTLPSpanExporter,
 )
@@ -12,13 +17,11 @@ from opentelemetry.sdk.trace.export import BatchSpanProcessor
 from opentelemetry.metrics import (
     CallbackOptions,
     Observation,
-    get_meter_provider,
     set_meter_provider,
 )
 from opentelemetry.sdk.metrics import MeterProvider
 from opentelemetry.sdk.metrics.export import PeriodicExportingMetricReader
 from opentelemetry.instrumentation.django import DjangoInstrumentor
-from opentelemetry.instrumentation.logging import LoggingInstrumentor
 
 
 def observable_counter_func(options: CallbackOptions) -> Iterable[Observation]:
@@ -27,14 +30,38 @@ def observable_counter_func(options: CallbackOptions) -> Iterable[Observation]:
 
 def setup_observability():
     trace.set_tracer_provider(TracerProvider())
-    otlp_exporter = OTLPSpanExporter(insecure=True)
+    otlp_exporter = OTLPSpanExporter()
     span_processor = BatchSpanProcessor(otlp_exporter)
     trace.get_tracer_provider().add_span_processor(span_processor)
 
-    exporter = OTLPMetricExporter(insecure=True)
+    exporter = OTLPMetricExporter()
     reader = PeriodicExportingMetricReader(exporter)
     provider = MeterProvider(metric_readers=[reader])
     set_meter_provider(provider)
 
     DjangoInstrumentor().instrument()
-    LoggingInstrumentor().instrument(set_logging_format=True)
+
+    ########
+    # Logs # - OpenTelemetry Logs are still in the "experimental" state, so function names may change in the future
+    ########
+    logging.basicConfig(level=logging.DEBUG)
+
+    # Initialize logging and an exporter that can send data to an OTLP endpoint by attaching OTLP handler to root logger
+    _logs.set_logger_provider(LoggerProvider())
+    # You can add handlers to custom loggers as well
+    logging.getLogger().addHandler(
+        LoggingHandler(
+            logger_provider=_logs.get_logger_provider().add_log_record_processor(
+                BatchLogRecordProcessor(OTLPLogExporter())
+            )
+        )
+    )
+
+    # Adding Handler to core.engine_api
+    logging.getLogger("core.engine_api").addHandler(
+        LoggingHandler(
+            logger_provider=_logs.get_logger_provider().add_log_record_processor(
+                BatchLogRecordProcessor(OTLPLogExporter())
+            )
+        )
+    )
