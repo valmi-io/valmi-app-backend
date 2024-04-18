04e14c1ff8a03ddbd4afd30cffb3eb8c7f4f6035
supradeep2819 <supradeep.c@valmi.io>
2024-04-18 12:07:22 +0530

    modified preview api

--- core/explore_api.py
+++ core/explore_api.py
@@ -8,7 +8,7 @@ import uuid
 import psycopg2
 from pydantic import Json
 from core.models import Account, Explore, Prompt, StorageCredentials, Workspace
-from core.schemas import DetailSchema, ExplorePreviewDataIn, ExploreSchema, ExploreSchemaIn
+from core.schemas import DetailSchema, ExploreSchema, ExploreSchemaIn
 from ninja import Router
 
 logger = logging.getLogger(__name__)
@@ -44,17 +44,16 @@ def create_explore(request, workspace_id,payload: ExploreSchemaIn):
             data["account"] = Account.objects.create(**account_info)
         logger.debug(data)
         explore = Explore.objects.create(**data)
+        
         return explore
     except Exception:
         logger.exception("explore creation error")
         return (400, {"detail": "The specific explore cannot be created."})
     
-@router.get("/workspaces/{workspace_id}/preview-data",response={200: Json, 400: DetailSchema})
-def create_sync(request, workspace_id,payload: ExplorePreviewDataIn):
-    data = payload.dict()
+@router.get("/workspaces/{workspace_id}/prompts/{prompt_id}/preview",response={200: Json, 400: DetailSchema})
+def create_sync(request, workspace_id,prompt_id):
     logger.info("data before creating")
-    logger.info(data)
-    prompt = Prompt.objects.get(id=data["prompt_id"])
+    prompt = Prompt.objects.get(id=prompt_id)
     storage_cred = StorageCredentials.objects.get(workspace_id=workspace_id)
     host_url = os.environ["DB_URL"]
     db_password = storage_cred.connector_config.get('password')
