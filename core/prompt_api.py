dcbc51b45c2f0be5d081a62f8800fc1762a14d29
supradeep2819 <supradeep.c@valmi.io>
2024-05-09 16:17:41 +0530

    feat: removed limit on records in liquid template

--- core/prompt_api.py
+++ core/prompt_api.py
@@ -56,8 +56,12 @@ def custom_serializer(obj):
     
 @router.post("/workspaces/{workspace_id}/prompts/{prompt_id}/preview",response={200: Json, 404: DetailSchema})
 def preview_data(request, workspace_id,prompt_id, prompt_req: PromptPreviewSchemaIn):
+    name,created_at = prompt_req.source_id.split("$")
+    logger.debug(name,created_at)
+    credential = Credential.objects.get(name=name,created_at=created_at)
+    source_id = Source.objects.get(credential_id = credential.id)
     prompt = Prompt.objects.get(id=prompt_id)
-    source_access_info = SourceAccessInfo.objects.get(source_id=prompt_req.source_id)
+    source_access_info = SourceAccessInfo.objects.get(source_id=source_id)
     storage_credentials = StorageCredentials.objects.get(id=source_access_info.storage_credentials.id)
     schema_name = storage_credentials.connector_config["schema"]
     table_name = f'{schema_name}.{prompt.table}'
