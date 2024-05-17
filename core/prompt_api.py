66a2712103b6bafb61a1905d5423c30a76b642a1
supradeep2819 <supradeep.c@valmi.io>
2024-05-17 13:27:28 +0530

    feat: added try except block for preview data endpoint

--- core/prompt_api.py
+++ core/prompt_api.py
@@ -15,6 +15,7 @@ logger = logging.getLogger(__name__)
 
 router = Router()
 
+
 @router.get("/", response={200: List[PromptSchemaOut], 400: DetailSchema})
 def get_prompts(request):
     try:
@@ -22,22 +23,23 @@ def get_prompts(request):
         connector_ids = list(Credential.objects.values('connector_id').distinct())
         connector_types = [connector['connector_id'] for connector in connector_ids]
         for prompt in prompts:
-            prompt["id"]  = str(prompt["id"])
+            prompt["id"] = str(prompt["id"])
             if prompt["type"] in connector_types:
                 prompt["enabled"] = True
             else:
                 prompt["enabled"] = False
         return prompts
     except Exception as err:
-        logger.exception("prompts listing error:"+ err)
+        logger.exception("prompts listing error:" + err)
         return (400, {"detail": "The list of prompts cannot be fetched."})
 
+
 @router.get("/workspaces/{workspace_id}/prompts/{prompt_id}", response={200: PromptByIdSchema, 400: DetailSchema})
-def get_prompt(request,workspace_id,prompt_id):
+def get_prompt(request, workspace_id, prompt_id):
     try:
         logger.debug("listing prompts")
         prompt = Prompt.objects.get(id=prompt_id)
-        if not PromptService.is_prompt_enabled(workspace_id,prompt):
+        if not PromptService.is_prompt_enabled(workspace_id, prompt):
             detail_message = f"The prompt is not enabled. Please add '{prompt.type}' connector"
             return 400, {"detail": detail_message}
         credential_info = Source.objects.filter(
@@ -69,29 +71,35 @@ def get_prompt(request,workspace_id,prompt_id):
         logger.exception("prompt listing error")
         return (400, {"detail": "The  prompt cannot be fetched."})
 
-    
+
 def custom_serializer(obj):
     if isinstance(obj, datetime.datetime):
         return obj.isoformat()
-    
-@router.post("/workspaces/{workspace_id}/prompts/{prompt_id}/preview",response={200: Json, 400: DetailSchema})
-def preview_data(request, workspace_id,prompt_id, prompt_req: PromptPreviewSchemaIn):
-    prompt = Prompt.objects.get(id=prompt_id)
-    if not PromptService.is_prompt_enabled(workspace_id,prompt):
+
+
+@router.post("/workspaces/{workspace_id}/prompts/{prompt_id}/preview", response={200: Json, 400: DetailSchema})
+def preview_data(request, workspace_id, prompt_id, prompt_req: PromptPreviewSchemaIn):
+    try:
+        prompt = Prompt.objects.get(id=prompt_id)
+        if not PromptService.is_prompt_enabled(workspace_id, prompt):
             detail_message = f"The prompt is not enabled. Please add '{prompt.type}' connector"
             return 400, {"detail": detail_message}
-    storage_credentials = StorageCredentials.objects.get(id=prompt_req.schema_id)
-    schema_name = storage_credentials.connector_config["schema"]
-    table_name = f'{schema_name}.{prompt.table}'
-    query = PromptService().build(table_name, prompt_req.time_window, prompt_req.filters)
-    logger.debug(query)
-    host_url = os.environ["DATA_WAREHOUSE_URL"]
-    db_password = storage_credentials.connector_config.get('password')
-    db_username = storage_credentials.connector_config.get('username')
-    conn = psycopg2.connect(host=host_url,port="5432",database="dvdrental",user=db_username,password=db_password)
-    cursor = conn.cursor()
-    cursor.execute(query)
-    items = [dict(zip([key[0] for key in cursor.description], row)) for row in cursor.fetchall()]
-    conn.commit()
-    conn.close()
-    return json.dumps(items, indent=4, default=custom_serializer)
+        storage_credentials = StorageCredentials.objects.get(id=prompt_req.schema_id)
+        schema_name = storage_credentials.connector_config["schema"]
+        table_name = f'{schema_name}.{prompt.table}'
+        query = PromptService().build(table_name, prompt_req.time_window, prompt_req.filters)
+        logger.debug(query)
+        host_url = os.environ["DATA_WAREHOUSE_URL"]
+        db_password = storage_credentials.connector_config.get('password')
+        db_username = storage_credentials.connector_config.get('username')
+        conn = psycopg2.connect(host=host_url, port="5432", database="dvdrental",
+                                user=db_username, password=db_password)
+        cursor = conn.cursor()
+        cursor.execute(query)
+        items = [dict(zip([key[0] for key in cursor.description], row)) for row in cursor.fetchall()]
+        conn.commit()
+        conn.close()
+        return json.dumps(items, indent=4, default=custom_serializer)
+    except Exception as err:
+        logger.exception(f"preview fetching error:{err}")
+        return (400, {"detail": "Data cannot be fetched."})
