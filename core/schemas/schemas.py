4a93fde44327d365ae428fa354aa61e21a532cc1
supradeep2819 <supradeep.c@valmi.io>
2024-06-18 18:52:06 +0530

    feat: Added time_window_enabled in schema

--- core/schemas/schemas.py
+++ core/schemas/schemas.py
@@ -81,14 +81,14 @@ class PromptSchema(ModelSchema):
     class Config(CamelSchemaConfig):
         model = Prompt
         model_fields = ["id", "name", "description", "type", "filters", "operators",
-                        "package_id", "gated", "query", "time_grain_enabled", "time_grain_enabled"]
+                        "package_id", "gated", "query", "time_grain_enabled", "time_window_enabled"]
 
 
 class PromptByIdSchema(ModelSchema):
     class Config(CamelSchemaConfig):
         model = Prompt
         model_fields = ["id", "name", "description", "type", "filters", "operators",
-                        "package_id", "gated", "query", "time_grain_enabled", "time_grain_enabled"]
+                        "package_id", "gated", "query", "time_grain_enabled", "time_window_enabled"]
     time_grain: List[TimeGrain] = None
     schemas: List[Dict]
 
