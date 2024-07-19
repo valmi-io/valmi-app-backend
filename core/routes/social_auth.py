cdb18195a8b74f43e08b88d01869b70c2b588a6e
supradeep2819 <supradeep.c@valmi.io>
2024-07-19 12:59:20 +0530

    feat: add user service for managing OAuth API keys and creating channel topics

--- core/routes/social_auth.py
+++ core/routes/social_auth.py
@@ -1,24 +1,17 @@
 
-import binascii
 import json
 import logging
 import os
 import uuid
 from urllib.parse import urlparse
-from typing import Dict, Tuple
 import psycopg2
-from ninja import Router, Schema
+from ninja import Router
 from pydantic import Json
 from rest_framework.authtoken.models import Token
 
-from core.models import OAuthApiKeys, Organization, Storefront, User, Workspace, ValmiUserIDJitsuApiToken, ChannelTopics, WorkspaceStorefront
-from core.routes.stream_api import create_obj, get_objs
+from core.models import Organization, User, Workspace
 from core.schemas.schemas import DetailSchema, SocialAuthLoginSchema
-
-from django.db import connection
-
-import hashlib
-
+from core.services.user import UserService
 
 from decouple import config
 
@@ -29,35 +22,7 @@ router = Router()
 logger = logging.getLogger(__name__)
 
 
-class AuthToken(Schema):
-    auth_token: str
-
 # TODO took from serializaer. move to utils
-
-
-def generate_key():
-    return binascii.hexlify(os.urandom(20)).decode()
-
-
-def patch_jitsu_user(user, workspace):
-    jitsu_defaultSeed = "dea42a58-acf4-45af-85bb-e77e94bd5025"
-    with connection.cursor() as cursor:
-        cursor.execute('INSERT INTO "newjitsu"."Workspace" ( id, name ) VALUES ( %s, %s ) ',
-                       [str(workspace.id), workspace.name])
-        cursor.execute('INSERT INTO "newjitsu"."UserProfile" (id, name, email, "loginProvider", "externalId") VALUES (%s, %s, %s, %s, %s) ',
-                       [str(user.id), user.username, user.email, "valmi", str(user.id)])
-        cursor.execute('INSERT INTO "newjitsu"."WorkspaceAccess" ("workspaceId", "userId") VALUES ( %s, %s) ',
-                       [str(workspace.id), str(user.id)])
-        apiKeyId = generate_key()
-        apiKeySecret = generate_key()
-        randomSeed = generate_key()
-        hash = randomSeed + "." + \
-            hashlib.sha512((apiKeySecret + randomSeed + jitsu_defaultSeed).encode('utf-8')).hexdigest()
-        cursor.execute('INSERT INTO "newjitsu"."UserApiToken" (id, hint, hash, "userId") VALUES (%s, %s, %s, %s)',
-                       [apiKeyId, apiKeySecret[0:3] + '*' + apiKeySecret[-3:], hash, str(user.id)])
-        connection.commit()
-    # store jitsu apitoken for valmi user
-    ValmiUserIDJitsuApiToken.objects.create(user=user, api_token=apiKeyId + ":" + apiKeySecret, workspace=workspace)
 
 
 # TODO response for bad request, 400
@@ -90,25 +55,15 @@ def login(request, payload: SocialAuthLoginSchema):
         workspace = Workspace.objects.get(organization_id=result)
         workspace_id = workspace.id
         # this below function creates oauth token if not exists for an existing user else updates , creates new jitsu token if not exists
-        manage_oauth_and_jitsu_tokens(user, account, workspace)
+        UserService.manage_oauth_and_jitsu_tokens(user, account, workspace)
         # this below function creates channel topics for chatbot if not exists for an existing user
-        link_response = create_channel_topics(request, user, workspace_id)
-        if link_response:
-            channel_topic = {
-                "write_key": link_response[0],
-                "link_id": link_response[1],
-                "id": str(uuid.uuid4()),
-                "channel": "chatbox",
-                "storefront": "chitumalla-store",
-                "workspace": workspace
-            }
-            ChannelTopics.objects.create(**channel_topic)
+        UserService.create_channel_topics(request, user, workspace_id)
     except User.DoesNotExist:
         user = User()
         user.email = user_data["email"]
         user.username = user_data["name"]
         user.meta = user_data['meta']
-        user.password = generate_key()
+        user.password = UserService.generate_key()
         user.save(force_insert=True)
         org = Organization(name="Default Organization", id=uuid.uuid4())
         org.save()
@@ -117,26 +72,10 @@ def login(request, payload: SocialAuthLoginSchema):
         user.save()
         user.organizations.add(org)
         if config("ENABLE_JITSU", default=False, cast=bool):
-            patch_jitsu_user(user, workspace)
-        link_response = create_channel_topics(request, user, workspace.id)
-        store_front_payload = {
-            "platform": "shopify",
-            "id": "chitumalla-store"
-        }
-        store_front = Storefront.objects.create(**store_front_payload)
-        workspace_storefront_payload = {
-            "workspace": workspace,
-            "storefront": store_front
-        }
-        workspace_storefront = WorkspaceStorefront.objects.create(**workspace_storefront_payload)
-        channel_topic = {
-            "write_key": link_response[0],
-            "link_id": link_response[1],
-            "channel": "chatbox",
-            "storefront": store_front,
-            "workspace": workspace
-        }
-        ChannelTopics.objects.create(**channel_topic)
+            # if jitsu enabled create jitsu account as well for new userss
+            UserService.patch_jitsu_user(user, workspace)
+        # create the channle topics for new users
+        UserService.create_channel_topics(request, user, workspace.id)
 
     token, _ = Token.objects.get_or_create(user=user)
     user_id = user.id
@@ -176,92 +115,3 @@ def login(request, payload: SocialAuthLoginSchema):
         return json.dumps(response)
     except Exception as e:
         return (400, {"detail": e.message})
-
-
-def manage_oauth_and_jitsu_tokens(user, account, workspace):
-    oauth = OAuthApiKeys.objects.filter(workspace=workspace.id, type='GOOGLE_LOGIN').first()
-    if oauth:
-        oauth.oauth_config = {
-            "access_token": account["access_token"],
-            "refresh_token": account["refresh_token"],
-            "expires_at": account["expires_at"]
-        }
-        oauth.save()
-    else:
-        oauth = OAuthApiKeys(
-            workspace=workspace,
-            type='GOOGLE_LOGIN',
-            id=uuid.uuid4(),
-            oauth_config={
-                "access_token": account["access_token"],
-                "refresh_token": account["refresh_token"],
-                "expires_at": account["expires_at"]
-            }
-        )
-        oauth.save()
-    jitsu_token_exists = ValmiUserIDJitsuApiToken.objects.filter(user_id=user.id)
-    if not jitsu_token_exists.exists():
-        if config("ENABLE_JITSU", default=False, cast=bool):
-            patch_jitsu_user(user, workspace)
-
-
-def generate_write_key() -> Dict[str, str]:
-    id = str(uuid.uuid4())
-    buffer = uuid.uuid4().bytes
-    plaintext = binascii.hexlify(buffer).decode('utf-8')
-    hint = plaintext[:3] + '*' + plaintext[-3:]
-
-    return {'id': id, 'plaintext': plaintext, 'hint': hint}
-
-
-def create_channel_topics(request, user, workspace_id) -> Tuple[str, str]:
-    channel_topic = ChannelTopics.objects.filter(workspace_id=workspace_id, channel="chatbox").first()
-    if channel_topic:
-        return []
-    source = {
-        "id": str(uuid.uuid4()),
-        "type": "stream",
-        "workspaceId": str(workspace_id),
-        "name": "chatbot",
-        "domains": []
-    }
-    publicKeys = []
-    publickey = generate_write_key()
-    publicKeys.append(publickey)
-    source["publicKeys"] = publicKeys
-    privateKeys = []
-    privatekey = generate_write_key()
-    privateKeys.append(privatekey)
-    logger.debug("*" * 80)
-    logger.debug(f"{privatekey['id']}:{privatekey['plaintext']}")
-    source["privateKeys"] = privateKeys
-    concated_public_key = f"{publickey['id']}:{publickey['plaintext']}"
-    config_type = "stream"
-    create_obj(request, workspace_id, config_type, source)
-    config_type = "destination"
-    destination = {
-        "id": str(uuid.uuid4()),
-        "type": "destination",
-        "workspaceId": str(workspace_id),
-        "destinationType": "bulkerdevnull",
-        "name": "destination-chatbot"
-    }
-    create_obj(request, workspace_id, config_type, destination)
-    sources_json = get_objs(request, workspace_id, "stream")
-    sources = json.loads(sources_json)
-    destinations_json = get_objs(request, workspace_id, "destination")
-    destinations = json.loads(destinations_json)
-    link = {
-        "fromId": sources["objects"][0]["id"],
-        "toId": destinations["objects"][0]["id"],
-        "type": "push",
-        "workspaceId": str(workspace_id)
-    }
-    data = {
-        "mode": "batch"
-    }
-    link["data"] = data
-    config_type = "link"
-    link_json = create_obj(request, workspace_id, config_type, link)
-    link_response = json.loads(link_json)
-    return [concated_public_key, link_response["id"]]
